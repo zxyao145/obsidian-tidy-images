b8c0209295f5af9b7554beaf77d31a3b21066131
zxyao145 <zxyao145@gmail.com>
2024-12-22 00:58:14 +0800

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,164 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Notice,
+	Plugin,
+} from "obsidian";
+import * as path from "path";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TidyImagesPluginSettings {
+	imageNamePrefix: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: TidyImagesPluginSettings = {
+	imageNamePrefix: "${fileName}",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TidyImagesPlugin extends Plugin {
+	settings: TidyImagesPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		console.log("plugin setting", this.settings);
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"outdent",
+			"Tidy Images",
+			(evt: MouseEvent) => {
+				this.tidyImages()
+				// Called when the user clicks the icon.
+				// new Notice("This is a notice!");
+			}
+		);
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("tidy-images-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("tidy images...");
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: "tidy-images-rename-images",
+			name: "Tidy Images: Rename images based on the order they appear",
+			checkCallback: (checking) => {
+				console.log(checking);
+				if (!checking) {
+					this.tidyImages();
 				}
+				return true;
+			},
+		});
+	}
+
+	async tidyImages() {
+		const file = this.app.workspace.getActiveFile();
+		if (!file) {
+			return false;
+		}
+
+		const vault = this.app.vault;
+		const fileContent = await vault.read(file);
+		const imageRegex = /!\[.*?\]\((.*?)\)/g;
+
+		// assets/test%20测试/file-20241221234708379.png
+		const images = [];
+		let match;
+		while ((match = imageRegex.exec(fileContent)) !== null) {
+			images.push(match[1]);
+		}
+		console.log("images", images);
+
+		if (images.length === 0) {
+			new Notice("No images found in the file.");
+			return;
+		}
+
+		// post/test 测试.md
+		// console.log("file.path", file.path);
+		// post
+		const fileDir = path.dirname(file.path);
+		// test 测试
+		const fileName = path.basename(file.path, ".md");
+
+		let newContent = fileContent;
+		// E:\ob
+		//@ts-ignore
+		const vaultBasePath = this.app.vault.adapter.basePath;
+
+		for (let i = 0; i < images.length; i++) {
+			// assets/test%20测试/file-20241221234708379.png
+			let oldRealImagePath = images[i];
+			const oldImagePath = oldRealImagePath.replaceAll("%20", " ");
+
+			// E:\ob\post\assets\test 测试\file-20241221234708379.png
+			const oldImageFullPath = path.resolve(
+				vaultBasePath,
+				fileDir,
+				oldImagePath
+			);
+
+			// post/assets/test 测试/file-20241221234708379.png
+			const oldVaultPath = path.relative(vaultBasePath, oldImageFullPath)
+					.replaceAll('\\', "/");
+			if (!(await vault.adapter.exists(oldVaultPath))) {
+				new Notice(`Image not found: ${oldVaultPath}`);
+				continue;
 			}
-		});
+			// E:\ob\post\assets\test 测试\
+			const imgDirName = path.dirname(oldImageFullPath);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			// .png
+			const ext = path.extname(oldImagePath);
+			let prefix = this.settings.imageNamePrefix.replace(
+				"${fileName}",
+				fileName
+			);
+			const newImageName = prefix + `-${i + 1}${ext}`;
+			const newImagePath = path.join(imgDirName, newImageName);
+			const newVaultPath = path
+				.relative(vaultBasePath, newImagePath)
+				.replaceAll("\\", "/");
+				
+			// post/assets/test%20测试/test%20测试-1.png
+			const newRealImagePath = path
+				.relative(path.join(vaultBasePath, fileDir), newImagePath)
+				.replaceAll(' ', "%20")
+				.replaceAll('\\', "/");
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			// Rename the image file
+			try {
+				await vault.adapter.rename(
+					oldVaultPath,
+					newVaultPath
+				);
+				newContent = newContent.replace(oldRealImagePath, newRealImagePath);
+			} catch (error) {
+				console.error(
+					`Failed to rename image: ${oldImagePath} -> ${newImagePath}`,
+					error
+				);
+				new Notice(`Failed to rename image: ${oldImagePath}`);
+			}
+		}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Update the file with the new content
+		await vault.modify(file, newContent);
+		new Notice("Images tidied finished.");
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
